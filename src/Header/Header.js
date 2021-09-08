861ba1cdb8ef2e70291f5d21935ff1a9832db714
souleymane kone <desolo74@gmail.com>
2021-09-08 04:02:34 -0700

    addComponent

---
+++ src/Header/Header.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import TokenService from '../services/TokenService';
+import './Header.css';
+
+// import AuthContext from '../contexts/AuthContext';
+import ApiContext from '../contexts/ApiContext';
+
+export default class Header extends React.Component {
+
+  static contextType = ApiContext;
+
+  handleLogoutClick = () => {
+    TokenService.clearAuthToken();
+    this.context.setUser(null);
+    console.log(this.context);
+  }
+
+
+  
+  renderLogoutLink() {
+    return (
+      <div className='site-nav__dir--logged-in'>
+          {' '}
+        <Link to={'/addproduct'} className='addProductlink'>
+           Product
+          </Link>
+          {' '}
+          <Link onClick={this.handleLogoutClick} to='/' className='logoutlink'>
+          Logout
+          </Link>
+
+      </div>
+    );
+  };
+
+  renderLoginLink() {
+    return (
+      <div className='site-nav__dir'>
+        <Link to='/register' className='registerLink'> Register</Link>
+        <Link to='/login' className='loginLink'> Login </Link>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <nav className='site-nav'>
+        <h1>
+          <Link className="titlelink"  to='/'>
+          Blood Pressure Management
+          </Link>
+        
+        </h1>
+        
+        {
+           this.context.user
+          // TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()
+        }
+      </nav>
+    );
+  };
+};
+
+
+
+
+
+
+
